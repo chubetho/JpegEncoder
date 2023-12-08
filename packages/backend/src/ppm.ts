e93caaf339b2f6677621978e8636d7ceb7a8d1e4
phatnguyen <phatnguyen@gmx.de>
2023-12-08 13:02:04 +0100

    cp

--- packages/backend/src/ppm.ts
+++ packages/backend/src/ppm.ts
@@ -9,26 +9,48 @@ export function readPpm(filePath: string) {
   const [width, height] = size.split(' ')
   const rows = _rows.filter(r => r.trimStart()[0] !== '#')
 
-  return { format, width, height, maxColor, rows }
-}
-
-export function parsePpm(rows: string[]) {
-  const R: Channel = []
-  const G: Channel = []
-  const B: Channel = []
+  const R: number[] = []
+  const G: number[] = []
+  const B: number[] = []
+  const Y: number[] = []
+  const Cb: number[] = []
+  const Cr: number[] = []
 
   rows.forEach((r, x) => {
     const row = r.split(' ').filter(Boolean).map(Number)
+    const rowIdx = x * rows.length
 
-    R[x] ||= []
-    G[x] ||= []
-    B[x] ||= []
     for (let i = 0; i <= row.length - 3; i += 3) {
-      R[x][i / 3] = row[i]
-      G[x][i / 3] = row[i + 1]
-      B[x][i / 3] = row[i + 2]
+      const r = row[i]
+      const g = row[i + 1]
+      const b = row[i + 2]
+
+      const y = 0.299 * r + 0.587 * g + 0.114 * b - 128
+      const cb = -0.1687 * r + -0.3312 * g + 0.5 * b
+      const cr = 0.5 * r + -0.4186 * g + -0.0813 * b
+
+      R[rowIdx + i / 3] = r
+      G[rowIdx + i / 3] = g
+      B[rowIdx + i / 3] = b
+
+      Y[rowIdx + i / 3] = y < -128 ? -128 : y > 127 ? 127 : y
+      Cb[rowIdx + i / 3] = cb < -128 ? -128 : cb > 127 ? 127 : cb
+      Cr[rowIdx + i / 3] = cr < -128 ? -128 : cr > 127 ? 127 : cr
     }
   })
 
-  return { R, G, B }
+  return {
+    R,
+    G,
+    B,
+    Y,
+    Cb,
+    Cr,
+    metadata: {
+      width,
+      height,
+      maxColor,
+      format,
+    },
+  }
 }
--- packages/backend/src/ycbcr.ts
+++ packages/backend/src/ycbcr.ts
@@ -1,31 +1,3 @@
-export function transformRgbToYcbcrChannels(R: Channel, G: Channel, B: Channel) {
-  const Y: Channel = []
-  const Cb: Channel = []
-  const Cr: Channel = []
-
-  R.forEach((row, rowIndex) => {
-    Y[rowIndex] ||= []
-    Cb[rowIndex] ||= []
-    Cr[rowIndex] ||= []
-
-    row.forEach((_, colIndex) => {
-      const r = R[rowIndex][colIndex]
-      const g = G[rowIndex][colIndex]
-      const b = B[rowIndex][colIndex]
-
-      const y = 0.299 * r + 0.587 * g + 0.114 * b
-      const cb = -0.1687 * r + -0.3312 * g + 0.5 * b + 128
-      const cr = 0.5 * r + -0.4186 * g + -0.0813 * b + 128
-
-      Y[rowIndex][colIndex] = ~~y
-      Cr[rowIndex][colIndex] = ~~cr
-      Cb[rowIndex][colIndex] = ~~cb
-    })
-  })
-
-  return { Y, Cb, Cr }
-}
-
 export function subsamplingCbCr(Cb: Channel, Cr: Channel) {
   const subCb: Channel = []
   const subCr: Channel = []
